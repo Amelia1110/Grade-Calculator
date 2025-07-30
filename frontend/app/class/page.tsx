aa9888e6eb17c628289815e78d07e108785d8645
Amelia1110 <ameliasongjiyun@hotmail.com>
2025-07-30 01:57:10 -0400

    api connection works!

--- frontend/app/class/page.tsx
+++ frontend/app/class/page.tsx
@@ -7,21 +7,25 @@ export default function ClassPage() {
     const [assessmentTypes, setAssessmentTypes] = useState<any>();
 
     useEffect(() => {
-        axios.get('https://grade-calculator-onxm.onrender.com/admin')
+        axios.get('https://grade-calculator-onxm.onrender.com/assessment-types/')
             .then((res: any) => {
                 let data = res.data;
                 setAssessmentTypes(data);
             })
             .catch((err: any) => {})
+    }, [])
+
+    useEffect(() => {
+        console.log(assessmentTypes)
     })
 
     return (
         <div>
-            {assessmentTypes?.map((item: any) => {
+            {assessmentTypes?.map((item: any) => (
                 <div className="bg-gray-500 rounded-full w-40 m-10 text-white p-2">
                     {item.name}
                 </div>
-            })}
+            ))}
         </div>
     )
 }
